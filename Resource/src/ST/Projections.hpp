af36bfec35c09249d6de6b073744233fe3e771d9
Chih-Chen Kao <elros.morlin@gmail.com>
2017-05-10 23:28:24 +0800

    add projections
    add projections

--- Resource/src/ST/Projections.hpp
+++ Resource/src/ST/Projections.hpp
@@ -8,36 +8,9 @@
 namespace ST {
 
 
-	cv::Mat cylinderProjection(const cv::Mat& image, double focalLength) {
+	cv::Mat cylinderProjection(const cv::Mat& image, double focalLength, bool grayScaleFlag); 
+	
 
-		double hWidth = (double)image.size().width / 2;
-		double hHeight = (double)image.size().height / 2;
-
-
-		size_t imgWidth = focalLength * std::atan(hWidth / focalLength);
-		size_t imgHeight = hHeight;
-
-		cv::Mat projectionImage (cv::Size(imgWidth * 2, imgHeight * 2), CV_8UC3);
-
-		
-		for (size_t j = 0; j < image.size().height; ++j) {
-			for (size_t i = 0; i < image.size().width; ++i) {
-
-
-				// yet to be done 
-
-
-
-			}
-		}
-
-
-
-
-
-		return projectionImage;
-
-	}
 
 
 }
